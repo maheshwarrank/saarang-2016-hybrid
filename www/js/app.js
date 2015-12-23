512d797da1377917cb1a4e4bfde6c998b7f720a5
Mahesh <maheshwarrank@gmail.com>
2015-12-23 19:37:22 +0530

    added map

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('saarang2016App', ['ionic', 'saarang2016App.controllers','ngSanitize'])
+angular.module('saarang2016App', ['ionic', 'saarang2016App.controllers','ngSanitize'/*,'saarang2016App.directives'*/])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
