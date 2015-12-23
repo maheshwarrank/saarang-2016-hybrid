c1ed4fc38b178e2f657fc7b4b41fa8f8c2307983
Mahesh <maheshwarrank@gmail.com>
2015-12-24 00:28:28 +0530

    added sponsors api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,43 +4,43 @@ angular.module('saarang2016App.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  // // With the new view caching in Ionic, Controllers are only called
+  // // when they are recreated or on app start, instead of every page change.
+  // // To listen for when this page is active (for example, to refresh data),
+  // // listen for the $ionicView.enter event:
+  // //$scope.$on('$ionicView.enter', function(e) {
+  // //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+  // // Form data for the login modal
+  // $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+  // // Create the login modal that we will use later
+  // $ionicModal.fromTemplateUrl('templates/login.html', {
+  //   scope: $scope
+  // }).then(function(modal) {
+  //   $scope.modal = modal;
+  // });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+  // // Triggered in the login modal to close it
+  // $scope.closeLogin = function() {
+  //   $scope.modal.hide();
+  // };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+  // // Open the login modal
+  // $scope.login = function() {
+  //   $scope.modal.show();
+  // };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+  // // Perform the login action when the user submits the login form
+  // $scope.doLogin = function() {
+  //   console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+  //   // Simulate a login delay. Remove this and replace with your login
+  //   // code if using a login system
+  //   $timeout(function() {
+  //     $scope.closeLogin();
+  //   }, 1000);
+  // };
 })
 
 .service('loadDetails',function(){
@@ -98,9 +98,9 @@ angular.module('saarang2016App.controllers', [])
     window.localStorage.setItem('events',angular.toJson( $scope.events));
     var events = angular.fromJson(window.localStorage['events']);
     console.log(events);
-    $scope.events1 = $scope.events.slice(0,50);
+    $scope.events1 = events.slice(0,50);
     console.log($scope.events1);
-    $scope.events2 = $scope.events.slice(50,99);
+    $scope.events2 = events.slice(50,99);
     console.log($scope.events2);
 
   });
@@ -148,7 +148,30 @@ angular.module('saarang2016App.controllers', [])
 
 })
 
-.controller('SponsorsCtrl', function($scope,loadDetails){
+.controller('SponsorsCtrl', function($scope,$http){
+  var sponsReq = {
+    method : 'GET',
+    url: 'http://erp.saarang.org/api/mobile/display_spons/'
+  };
+  $http(sponsReq).then(function(response){
+    $scope.spons = response.data.data;
+    $scope.spons.sort(function(a,b){
+      if (a.priority > b.priority) {
+        return -1;
+      }
+      if (a.priority < b.priority) {
+        return 1;
+      }
+  // a must be equal to b
+        return 0;
+    });
+    console.log($scope.spons);
+    window.localStorage.setItem('sponsors',angular.toJson($scope.spons));
+    var sponsors = angular.fromJson(window.localStorage['sponsors']);
+    console.log(sponsors);
+    $scope.sponsors = sponsors;
+    console.log($scope.sponsors);
+  });
 })
 
 .controller('MapCtrl', function($scope, $ionicLoading) {
